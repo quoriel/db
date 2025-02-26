75991a24c4b5f72fff187e90e7df148d5b1369e1
Mlad <109038115+eolthar@users.noreply.github.com>
2025-02-26 23:36:22 +0200

    Update getVar.js

--- src/functions/var/getVar.js
+++ src/functions/var/getVar.js
@@ -42,8 +42,8 @@ exports.default = new NativeFunction({
         if (!types[type].json) return this.success(await get(type, name));
         const tupe = types[type].type;
         if (tupe === null) return this.stop();
-        let id = entity || ctx[tupe]?.id;
-        if (types[tupe].guild) id = `${id}${separator}${guild?.id || ctx.guild.id}`;
-        return this.success(await get(type, name, id));
+        entity ||= ctx[tupe]?.id;
+        if (types[tupe].guild) entity = entity + separator + (guild?.id || ctx.guild.id);
+        return this.success(await get(type, name, entity));
     }
 });
