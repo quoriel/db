4bdeb1ce3cc34545eb1148ba2a418ac51e5cf3ee
Mlad <109038115+eolthar@users.noreply.github.com>
2025-06-14 22:35:16 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 const { ForgeExtension } = require("@tryforge/forgescript");
 const { update } = require("./db");
+const package = require("../package.json");
 
 class QuorielDB extends ForgeExtension {
     name = "QuorielDB";
-    description = require("../package.json").description;
-    version = require("../package.json").version;
+    description = package.description;
+    version = package.version;
 
     init() {
         update();
