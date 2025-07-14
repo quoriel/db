c1eb3250c0f263acef0dc48169c1aa969e514a5b
Mlad <109038115+eolthar@users.noreply.github.com>
2025-07-14 23:39:56 +0300

    Add files via upload

---
+++ src/functions/var/fallbackVar.js
@@ -0,0 +1,31 @@
+const { NativeFunction, ArgType } = require("@tryforge/forgescript");
+const { variables } = require("../../db");
+
+exports.default = new NativeFunction({
+    name: "$fallbackVar",
+    version: "1.0.0",
+    description: "Loads merged data with default variables into an environment variable",
+    brackets: true,
+    unwrap: true,
+    args: [
+        {
+            name: "variable",
+            description: "Environment variable name",
+            type: ArgType.String,
+            required: true,
+            rest: false
+        },
+        {
+            name: "json",
+            description: "Data for merging",
+            type: ArgType.String,
+            required: true,
+            rest: false
+        }
+    ],
+    async execute(ctx, [variable, json]) {
+        const data = JSON.assign(variables, json);
+        ctx.setEnvironmentKey(variable, data);
+        this.success();
+    }
+});
