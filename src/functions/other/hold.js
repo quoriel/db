6e5e469fe9dff524bfc0c2ae2ae2bda63caa5819
Mlad <109038115+eolthar@users.noreply.github.com>
2025-10-26 17:58:17 +0300

    Add files via upload

--- src/functions/other/hold.js
+++ src/functions/other/hold.js
@@ -4,9 +4,9 @@ const { dbs, types, config } = require("../../db");
 exports.default = new NativeFunction({
     name: "$hold",
     description: "Applies a hold timer to prevent repeated actions",
-    version: "1.7.2",
+    version: "1.7.3",
     brackets: true,
-    unwrap: true,
+    unwrap: false,
     args: [
         {
             name: "type",
@@ -31,9 +31,9 @@ exports.default = new NativeFunction({
         },
         {
             name: "code",
-            description: "Code for execution",
+            description: "Code to execute",
             type: ArgType.String,
-            rest: false
+            required: false
         },
         {
             name: "entity",
@@ -48,39 +48,51 @@ exports.default = new NativeFunction({
             rest: false
         }
     ],
-    async execute(ctx, [type, name, duration, code, entity, guild]) {
-        const db = dbs.get(type);
+    async execute(ctx) {
+        const type = await this["resolveUnhandledArg"](ctx, 0);
+        if (!this["isValidReturnType"](type)) return type;
+        const name = await this["resolveUnhandledArg"](ctx, 1);
+        if (!this["isValidReturnType"](name)) return name;
+        const duration = await this["resolveUnhandledArg"](ctx, 2);
+        if (!this["isValidReturnType"](duration)) return duration;
+        const entityV = await this["resolveUnhandledArg"](ctx, 4);
+        if (!this["isValidReturnType"](entityV)) return entityV;
+        const db = dbs.get(type.value);
         if (!db) return this.success();
-        const view = types.get(type);
+        const view = types.get(type.value);
+        let entity = entityV?.value;
         if (!entity) {
             if (view.type === null) return this.success();
             entity = ctx[view.type]?.id;
         }
-        if (view.guild) entity = entity + config.entitySeparator + (guild?.id || ctx.guild.id);
+        if (view.guild) {
+            const guild = await this["resolveUnhandledArg"](ctx, 5);
+            if (!this["isValidReturnType"](guild)) return guild;
+            entity = entity + config.entitySeparator + (guild?.value?.id || ctx.guild.id);
+        }
         try {
             let data = db.get(entity) || {};
             if (!data.holds) data.holds = {};
-            const existing = data.holds[name];
-            if (existing && existing > Date.now()) {
-                if (code) {
-                    const content = await this.resolveCode(ctx, code);
-                    if (!this.isValidReturnType(content)) return content;
-                    ctx.container.content = content.value;
+            const existing = data.holds[name.value];
+            const now = Date.now();
+            if (existing && existing > now) {
+                const field = this.data.fields[3];
+                if (field) {
+                    const code = await this["resolveCode"](ctx, field);
+                    if (!this["isValidReturnType"](code)) return code;
+                    ctx.container.content = code.value;
                     await ctx.container.send(ctx.obj);
                 }
                 return this.stop();
             }
-            data.holds[name] = Date.now() + duration;
+            data.holds[name.value] = now + duration.value;
             if (config.eventUpdate) {
                 const old = db.get(entity);
                 await db.put(entity, data);
                 config.emitter.emit("recordUpdate", {
-                    type,
+                    type: type.value,
                     key: entity,
-                    value: {
-                        old,
-                        new: data
-                    }
+                    value: { old, new: data }
                 });
             } else {
                 await db.put(entity, data);
